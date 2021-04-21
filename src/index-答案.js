ac495aa54f812067367306494db8d2e1f9512516
s4s <s4s@start4start.com>
2021-04-21 17:41:10 +0800

    整理笔记

--- src/index-答案.js
+++ src/index-答案.js
@@ -7,6 +7,9 @@ import "./index.css";
 // ReactDOM.render(<App />, document.getElementById("root"));
 
 // 在脚手架中使用React，创建一个h1标签，包含‘Hello React 脚手架’文本
+const el = React.createElement("h1", null, "Hello React 脚手架");
+ReactDOM.render(el, document.getElementById("root"));
 
 // 在脚手架中使用JSX，创建一个h1标签，包含‘Hello React JSX 脚手架’文本
-
+// const el = <h1>Hello React JSX 脚手架</h1>
+// ReactDOM.render(el, document.getElementById("root"));
