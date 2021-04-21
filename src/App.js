bcbdba70cb09c41941bf2a053a7b8ad2cfc69b51
s4s <s4s@start4start.com>
2021-04-21 15:59:22 +0800

    App.js修改为函数组件

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import React from 'react'
+import React from "react";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        01-basic
-      </div>
-    )
-  }
+function App() {
+  return <div>01-basic</div>;
 }
 
-export default App
-
+export default App;
