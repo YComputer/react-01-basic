d35155f3f74bce46596849d571fc5f57bea1e045
s4s <s4s@start4start.com>
2021-04-25 00:45:55 +0800

    备课修改

--- src/02-脚手架组件-答案.js
+++ src/02-脚手架组件-答案.js
@@ -1,12 +1,19 @@
-import React from "react";
 /**
+ * 目标：定义一个函数组件并导出
  * 导入 React
  * 定义一个函数
  * 函数名首字母要大写，返回值是一个JSX描述的html结构
  * 导出组件
  */
+
+// 导入 React
+import React from "react"; 
+
+// 定义一个函数，函数名称首字母要大写
 function Hello() {
+  // 函数返回值是一个JSX描述的html结构
   return <h1>Hello React JSX 脚手架，这是文件导出的组件</h1>;
 }
 
+// 导出函数组件
 export default Hello;
